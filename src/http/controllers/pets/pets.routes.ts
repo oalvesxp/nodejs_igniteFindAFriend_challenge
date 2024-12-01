d1aba291e509edcebf13bb87e3d4a90245678bb1
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-12-01 19:37:19 -0300

    fix: rename route and add on app register

--- src/http/controllers/pets/pets.routes.ts
+++ src/http/controllers/pets/pets.routes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from 'fastify'
 
 import { createPetController } from './create-pets.controller'
 
-export async function orgsRoutes(app: FastifyInstance) {
-  app.post('/pets', createPetController)
+export async function petsRoutes(app: FastifyInstance) {
+  app.post('/orgs/pets', createPetController)
 }
