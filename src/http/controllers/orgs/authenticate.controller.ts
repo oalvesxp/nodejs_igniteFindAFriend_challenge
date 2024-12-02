7b893967aad7fe1badc13dc49d8157c815f4975c
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-12-02 13:29:50 -0300

    feat: add authenticate org controller

---
+++ src/http/controllers/orgs/authenticate.controller.ts
@@ -0,0 +1,40 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials.error'
+import { makeAuthenticateOrgUseCase } from '@/use-cases/factories/make-authenticate-org.use-case.factory'
+
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+const authenticateBodySchema = z.object({
+  email: z.string(),
+  password: z.string().min(6),
+})
+
+export async function authenticateOrgController(
+  req: FastifyRequest,
+  rep: FastifyReply,
+) {
+  const { email, password } = authenticateBodySchema.parse(req.body)
+
+  try {
+    const authenticateOrgUseCase = makeAuthenticateOrgUseCase()
+
+    const { org } = await authenticateOrgUseCase.execute({
+      email,
+      password,
+    })
+
+    const token = await rep.jwtSign({
+      sign: {
+        sub: org.id,
+      },
+    })
+
+    return rep.status(200).send({ token })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return rep.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+}
