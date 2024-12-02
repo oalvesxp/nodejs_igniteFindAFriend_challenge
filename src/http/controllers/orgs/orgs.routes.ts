83fa6964344607de1258a74703b1ec1245a20ee9
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-12-02 11:18:26 -0300

    fix: rename files

--- src/http/controllers/orgs/orgs.routes.ts
+++ src/http/controllers/orgs/orgs.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { createOrgController } from './create-org.controller'
+import { createOrgController } from './create.controller'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', createOrgController)
--- src/http/controllers/pets/pets.routes.ts
+++ src/http/controllers/pets/pets.routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
 
-import { createPetController } from './create-pets.controller'
+import { createPetController } from './create.controller'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/orgs/pets', createPetController)
