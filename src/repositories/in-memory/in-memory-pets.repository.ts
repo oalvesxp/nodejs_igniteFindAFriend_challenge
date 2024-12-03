19d3081906d80b6b3f03732c5286bcdbbdd6f455
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-12-03 15:49:55 -0300

    feat: add method to get pet by id

--- src/repositories/in-memory/in-memory-pets.repository.ts
+++ src/repositories/in-memory/in-memory-pets.repository.ts
@@ -5,6 +5,14 @@ import { randomUUID } from 'crypto'
 export class InMemoryPetsRepository implements PetsRepository {
   public items: Pet[] = []
 
+  async findById(id: string): Promise<Pet | null> {
+    const pet = this.items.find((item) => item.id === id)
+
+    if (!pet) return null
+
+    return pet
+  }
+
   async create(data: Prisma.PetUncheckedCreateInput): Promise<Pet> {
     const pet = {
       id: randomUUID(),
