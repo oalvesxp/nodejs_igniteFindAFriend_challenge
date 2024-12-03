aefcfc34daa9caf3cbd9a40ab50911dc1e7a0450
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-12-03 14:08:10 -0300

    fix: rename factory

--- src/use-cases/factories/make-fetch-nearby-org.use-case.factory.ts
+++ src/use-cases/factories/make-fetch-nearby-org.use-case.factory.ts
@@ -1,6 +1,6 @@
 import { PrismaOrgsRepository } from '@/repositories/prisma/prisma-orgs.repository'
 import { FetchNearbyOrgsUseCase } from '../fetch-nearby-orgs.use-case'
 
-export function makeCreateOrgUseCase() {
+export function makeFetchNearbyOrgUseCase() {
   return new FetchNearbyOrgsUseCase(new PrismaOrgsRepository())
 }
