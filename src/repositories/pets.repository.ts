797950f01b52fea4c82a7d8cd4c12d401bfe84a7
Osvaldo Alves <oalves.neto@outlook.com>
2025-01-05 10:55:57 -0300

    feat: add findAll method for pet repository

--- src/repositories/pets.repository.ts
+++ src/repositories/pets.repository.ts
@@ -1,6 +1,15 @@
 import { Pet, Prisma } from '@prisma/client'
 
+export interface FindAllParams {
+  city: string
+  age?: string
+  size?: string
+  energy_level?: string
+  environment?: string
+}
+
 export interface PetsRepository {
+  findAll(params: FindAllParams): Promise<Pet[]>
   findById(id: string): Promise<Pet | null>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
 }
--- src/repositories/prisma/prisma-pets.repository.ts
+++ src/repositories/prisma/prisma-pets.repository.ts
@@ -1,8 +1,26 @@
 import { prisma } from '@/lib/prisma'
 import { Prisma, Pet } from '@prisma/client'
-import { PetsRepository } from '../pets.repository'
+import { FindAllParams, PetsRepository } from '../pets.repository'
 
 export class PrismaPetsRepository implements PetsRepository {
+  async findAll(params: FindAllParams): Promise<Pet[]> {
+    const pets = await prisma.pet.findMany({
+      where: {
+        age: params.age,
+        size: params.size,
+        energy_level: params.energy_level,
+        org: {
+          city: {
+            contains: params.city,
+            mode: 'insensitive',
+          },
+        },
+      },
+    })
+
+    return pets
+  }
+
   async findById(id: string): Promise<Pet | null> {
     const pet = await prisma.pet.findUnique({
       where: {
