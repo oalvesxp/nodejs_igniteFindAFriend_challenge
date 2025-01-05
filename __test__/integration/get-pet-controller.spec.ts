7fecf19d28383bb43998f2cf8c8b0851fcda89bb
Osvaldo Alves <oalves.neto@outlook.com>
2025-01-05 10:27:50 -0300

    test: add integration test for get pet controller

---
+++ __test__/integration/get-pet-controller.spec.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import { app } from '@/app'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { makePet } from '__test__/factories/make-pet.factory'
+import { makeOrg } from '__test__/factories/make-org.factory'
+
+describe('[Integration] Get Pet Controller', () => {
+  beforeAll(() => {
+    app.ready()
+  })
+
+  afterAll(() => {
+    app.close()
+  })
+
+  it('Should be able to get a pet by ID', async () => {
+    const org = makeOrg()
+    await request(app.server).post('/orgs').send(org)
+
+    const authenticateResponse = await request(app.server)
+      .post('/orgs/sessions')
+      .send({
+        email: org.email,
+        password: org.password,
+      })
+
+    const createPetResponse = await request(app.server)
+      .post('/orgs/pets')
+      .set('Authorization', `Bearer ${authenticateResponse.body.token}`)
+      .send(makePet())
+
+    console.log(createPetResponse.body.pet.id)
+    const response = await request(app.server)
+      .get(`/orgs/pets/${createPetResponse.body.pet.id}`)
+      .set('Authorization', `Bearer ${authenticateResponse.body.token}`)
+
+    expect(response.status).toBe(200)
+  })
+})
--- src/http/controllers/pets/pets.routes.ts
+++ src/http/controllers/pets/pets.routes.ts
@@ -7,5 +7,5 @@ import { verifyJWT } from '@/http/middlewares/verify-jwt.middleware'
 
 export async function petsRoutes(app: FastifyInstance) {
   app.post('/orgs/pets', { onRequest: [verifyJWT] }, createPetController)
-  app.get('/orgs/pets/{id}', getPetController)
+  app.get('/orgs/pets/:id', getPetController)
 }
